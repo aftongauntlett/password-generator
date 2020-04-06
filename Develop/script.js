c8a4e964c5993aa37ced67f4f4443dd68c00c47b
Afton Gauntlett <afton.gauntlett@gmail.com>
2020-04-05 20:14:48 -0400

    updated char length prompt text

--- Develop/script.js
+++ Develop/script.js
@@ -18,18 +18,21 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 
   // created the prompt for length of password
-  var length = prompt("How long would you like your password, must be at least 8 characters.");
+  var length = prompt("How long would you like your password to be? (Must be between 8 -128 chararcters.)");
 
   // created the prompt for length of password
   var charset = prompt("Please choose: lowercase, uppercase, numeric, and/or special characters");
 
 
-
-
   // make it validate that at least one of those characters is present
 
+  if (charset = )
+    alert("Valid Password!")
 
-
+  }
+  else {
+    alert("Invalid Password!")
+  }
 
 
   // created an if statement to make sure the password is at least 8 characters long
@@ -43,9 +46,6 @@ function generatePassword() {
   }
 
 
-
-
-
   // created charset to include all possible characters for the generated password. Included toUpperCase.
   var charset = "abcdefghijklmnopqrstuvwxyz!@#$%^&* ()_ + ~`|}{[]\:;?><,./-=0123456789".toUpperCase() + "abcdefghijklmnopqrstuvwxyz";
 
@@ -57,8 +57,6 @@ function generatePassword() {
   for (var i = 0, n = charset.length; i < length; ++i) {
     password += charset.charAt(Math.floor(Math.random() * n));
   }
-
-  // create if statements
 
 
   return password;
